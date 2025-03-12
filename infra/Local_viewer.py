601c5aef0388d1e19a254548cd77a666092b2729
n <hhhello0507@dgsw.hs.kr>
2025-03-12 16:20:34 +0900

    a

--- infra/Local_viewer.py
+++ infra/Local_viewer.py
@@ -1,7 +1,11 @@
 import json
 from dataclasses import asdict
+from typing import List
+
+import pandas as pd
 
 from infra.Local import SkillLocal, CategoryLocal, ResultLocal
+from model.Results import Results
 
 base_path = './save/'
 
@@ -14,8 +18,28 @@ def show():
     # print('CategoryLocal')
     # print([i[0] for i in CategoryLocal.load()])
     # print()
+    data = []
 
-    print('ResultLocal')
-    result = ResultLocal.load()
-    # print([i[0] for i in ResultLocal.load()])
-    print()
+    result: List[(int, Results)] = ResultLocal.load()
+    for v in result[0][1].data:
+        row = {
+            'reward_total': v.reward_total,
+            'is_bookmark': v.is_bookmark,
+            'company': v.company,
+            'title_img': v.title_img,
+            'address': v.address,
+            'position': v.position,
+            'category_tag': v.category_tag,
+            'attraction_tags': v.attraction_tags,
+            'skill_tags': v.skill_tags,
+            'user_oriented_tags': v.user_oriented_tags,
+            'annual_from': v.annual_from,
+            'annual_to': v.annual_to,
+            'is_newbie': v.is_newbie,
+            'reward': v.reward
+        }
+        data.append(row)
+
+    df = pd.DataFrame(data)
+    df.to_excel('output.xlsx', index=False, engine='openpyxl')
+
